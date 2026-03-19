f2f2988eb2ebe04622c39a30d78fd12052ed645c
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2026-03-19 17:04:15 +0530

    chore(dependencies:update): update pnpm-lock.yaml with updated dependency versions
    Updated multiple dependencies, including `@astrojs/react`, `@astrojs/vue`, and others in `pnpm-lock.yaml` to their latest versions, ensuring compatibility and updated packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'astro/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import react from '@astrojs/react';
+
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss(), tsconfigPaths()],
+  },
+
+  integrations: [react(), vue()],
+  image: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'takshashilascs.com' },
+    ],
   },
 })
